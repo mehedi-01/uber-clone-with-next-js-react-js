29cc1f7fc5e7bb0125c58efb56c8010b6e2f2f80
Md Mehedi Hasan <45948603+mehedi-01@users.noreply.github.com>
2022-01-17 12:35:20 +0600

    added

---
+++ pages/components/Map.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useEffect } from 'react';
+import tw from "tailwind-styled-components"
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibWVoZWRpLTAxIiwiYSI6ImNreWRnZGR6eTBjb2Qyb3MyNHhsYXMzcjcifQ.Lfr2CciVtucqKU7Ki3Hifw';
+
+const Map = () => {
+
+    
+  useEffect(() => {
+    // if (map.current) return; // initialize map only once
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-99.29011, 39.39172],
+      zoom: 3,
+    });
+  });
+
+    return  <Wrapper id='map'></Wrapper>
+        
+    
+}
+
+export default Map
+
+const Wrapper = tw.div`
+flex-1
+
+`
--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import tw from "tailwind-styled-components"
-import mapboxgl from 'mapbox-gl';
+import Map from './components/Map'
 
-mapboxgl.accessToken = 'pk.eyJ1IjoibWVoZWRpLTAxIiwiYSI6ImNreWRnZGR6eTBjb2Qyb3MyNHhsYXMzcjcifQ.Lfr2CciVtucqKU7Ki3Hifw';
+
+//mapboxgl.accessToken = 'pk.eyJ1IjoibWVoZWRpLTAxIiwiYSI6ImNreWRnZGR6eTBjb2Qyb3MyNHhsYXMzcjcifQ.Lfr2CciVtucqKU7Ki3Hifw';
 
 
 export default function Home() {
-
-  useEffect(() => {
-    // if (map.current) return; // initialize map only once
-    const map = new mapboxgl.Map({
-      container: "map",
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-99.29011, 39.39172],
-      zoom: 3,
-    });
-  });
 
   // const map = new mapboxgl.Map({
   //   container: 'map', // container ID
@@ -29,7 +20,7 @@ export default function Home() {
 
   return (
     <Wrapper>
-      <Map id='map'></Map>
+      <Map />
       <ActionItems>Start</ActionItems>
     </Wrapper>
   )
@@ -37,11 +28,6 @@ export default function Home() {
 
 const Wrapper = tw.div`
 flex flex-col bg-red-300 h-screen
-
-`
-
-const Map = tw.div`
-bg-red-500 flex-1
 
 `
 
